20b79ad8cc053c5a5c9652c41e9589fe43fff222
Datthehooman <datbui13579@gmail.com>
2025-10-08 21:07:30 +0700

    tạo login, sửa chitetsanpham

--- app/pages/san-pham/[slug].vue
+++ app/pages/san-pham/[slug].vue
@@ -1,5 +1,2 @@
-<template>
-  <div class="nut text-primary p-4 m-4">nut</div>
-  <div class="nut text-primary p-4 m-4">nut</div>
-</template>
+<template>chitietsanpham</template>
 <script lang="ts"></script>
