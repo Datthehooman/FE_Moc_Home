2f09b24f1f7c5a1e394489831be3beae46615062
Datthehooman <datbui13579@gmail.com>
2025-10-08 20:44:39 +0700

    nâng phiên bản nuxt ui, thêm icon, thêm logo

--- app/components/layouts/Main/Header.vue
+++ app/components/layouts/Main/Header.vue
@@ -1,2 +1,84 @@
-<template><div>Header</div></template>
-<script setup lang="ts"></script>
+<template>
+  <UHeader>
+    <template #left>
+      <NuxtImg src="/logo.png" />
+      <UNavigationMenu :items="items" />
+    </template>
+
+    <template #right>
+      <UInput
+        color="primary"
+        :highlight="true"
+        trailing-icon="i-lucide-search"
+        size="md"
+        variant="outline"
+        placeholder="Tìm kiếm..."
+      />
+
+      <UButton
+        color="primary"
+        variant="ghost"
+        to="https://github.com/nuxt/ui"
+        icon="i-lucide-heart"
+        aria-label="Sản phẩm yêu thích"
+      />
+      <UButton
+        color="primary"
+        variant="ghost"
+        to="https://github.com/nuxt/ui"
+        icon="i-lucide-shopping-cart"
+        aria-label="Giỏ hàng"
+      />
+      <UButton
+        color="primary"
+        variant="ghost"
+        to="https://github.com/nuxt/ui"
+        icon="i-lucide-user"
+        aria-label="Thông tin cá nhân"
+      />
+    </template>
+  </UHeader>
+</template>
+<script setup lang="ts">
+  import type { NavigationMenuItem } from "@nuxt/ui";
+
+  const route = useRoute();
+
+  const items = computed<NavigationMenuItem[]>(() => [
+    {
+      label: "Trang chủ",
+      to: "/docs/getting-started",
+      active: route.path.startsWith("/docs/getting-started"),
+    },
+    {
+      label: "Về chúng tôi",
+      to: "/docs/components",
+      active: route.path.startsWith("/docs/components"),
+    },
+    {
+      label: "Tài khoản",
+      to: "https://go.nuxt.com/figma-ui",
+      target: "_blank",
+    },
+    {
+      label: "Menu",
+      to: "https://github.com/nuxt/ui/releases",
+      target: "_blank",
+    },
+    {
+      label: "Cửa hàng",
+      to: "https://github.com/nuxt/ui/releases",
+      target: "_blank",
+    },
+    {
+      label: "Blog",
+      to: "https://github.com/nuxt/ui/releases",
+      target: "_blank",
+    },
+    {
+      label: "Liên hệ",
+      to: "https://github.com/nuxt/ui/releases",
+      target: "_blank",
+    },
+  ]);
+</script>
---
+++ app/pages/san-pham/[slug].vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="nut text-primary p-4 m-4">nut</div>
+  <div class="nut text-primary p-4 m-4">nut</div>
+</template>
+<script lang="ts"></script>
