37502ccdc19566ebd4a9466c33da0ea67ea73a3d
Le-Phung-Tien-Quan <quanlptps39861@gmail.com>
2025-10-17 14:02:12 +0700

    Thêm trang giỏ hàng và thanh toán, sửa lại import default, thêm sản phẩm liên quan chi tiết sản phẩm.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,7 +3,7 @@
     <LayoutsMainHeader />
     <div class="bg-[#f5f7fe]/50 dark:bg-[#0f172a]">
       <div>
-        <Breadcrumb v-if="route.path !== '/'" />
+        <LayoutsMainBreadcrumb v-if="route.path !== '/'" />
         <slot />
       </div>
     </div>
@@ -12,10 +12,5 @@
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router'
-import LayoutsMainHeader from '~/components/layouts/Main/Header.vue'
-import LayoutsMainFooter from '~/components/layouts/Main/Footer.vue'
-import Breadcrumb from '~/components/layouts/Main/Breadcrumb.vue'
-
 const route = useRoute()
 </script>
