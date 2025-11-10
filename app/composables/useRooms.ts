1f51dafa4d3502491412668442bd903d67777270
Datthehooman <datbui13579@gmail.com>
2025-11-10 20:04:27 +0700

    sửa lại base url

--- app/composables/useRooms.ts
+++ app/composables/useRooms.ts
@@ -9,7 +9,7 @@ export function useRooms() {
 
     try {
       const { data, error: fetchError } = await useCustomFetch<RoomApiResponse>(
-        "http://api.mocfurni.shop/api/client/rooms",
+        "/api/client/rooms",
         { method: "GET" }
       );
 
